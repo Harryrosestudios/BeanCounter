d196359ce9639f7010b9987d00fb53aee46cff74
Harry Rose <harry@harryrosestudios.com>
2025-07-04 10:22:43 +0100

    Initial Commit

---
+++ program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BeanCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello coffee!");
+        }
+    }
+}
+
